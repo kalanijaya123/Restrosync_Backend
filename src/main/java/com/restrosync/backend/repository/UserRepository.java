767a4e11a4cb81cebcbf782e73c03bcefad47623
kalani Jayasundara <167794802+kalanijaya123@users.noreply.github.com>
2025-11-13 21:39:57 +0530

    Added User repositary

---
+++ src/main/java/com/restrosync/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.restrosync.backend.repository;
+
+import com.restrosync.backend.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
