3747477aadf80f9cae1826e2fbfc6246e21defaa
kalani Jayasundara <167794802+kalanijaya123@users.noreply.github.com>
2025-11-13 17:28:28 +0530

    Added user model

---
+++ src/main/java/com/restrosync/backend/model/User.java
@@ -0,0 +1,14 @@
+package com.restrosync.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("users")
+public record User(
+        @Id String id,
+        String username,
+        String email,
+        String password
+
+) {
+}
