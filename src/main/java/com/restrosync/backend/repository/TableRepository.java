3c8a50a67ea29089b05658aba353de7b3b5a3639
kalani Jayasundara <167794802+kalanijaya123@users.noreply.github.com>
2025-11-13 21:41:53 +0530

    Added Table Repository

---
+++ src/main/java/com/restrosync/backend/repository/TableRepository.java
@@ -0,0 +1,7 @@
+package com.restrosync.backend.repository;
+
+import com.restrosync.backend.model.Table;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TableRepository extends MongoRepository<Table, String> {
+}
